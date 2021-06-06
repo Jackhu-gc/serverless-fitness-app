76e6c47fb07f0dcda53e73932f7954d002a06313
Ezclap <jackisplendid@gmail.com>
2021-06-06 22:33:33 +1000

    added category, added search in category

--- fitness-api/api/workout.js
+++ fitness-api/api/workout.js
@@ -1,38 +1,109 @@
 const AWS = require('aws-sdk');
-
 AWS.config.setPromisesDependency(require('bluebird'));
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-exports.get = (event, context, callback) => {
-  var params = {
-    TableName: process.env.PRE_DEFINED_WORKOUT_TABLE,
+exports.get = async (event, context, callback) => {
+  const { category } = event.pathParameters;
+  let { anyinTable } = event.queryStringParameters;
 
+  let params = {
+    TableName: process.env.PRE_DEFINED_WORKOUT_TABLE,
+    KeyConditionExpression: 'category = :pk',
+    ExpressionAttributeValues: {
+      ':pk': category,
+    },
     ProjectionExpression:
       'Id, workoutName, workoutTime, workoutDetails, category',
   };
 
-  console.log('Scanning Open Workout table.');
+  console.log('Query Workout table.');
 
-  const onScan = (err, data) => {
-    if (err) {
-      console.log(
-        'Scan failed to load data. Error JSON:',
-        JSON.stringify(err, null, 2)
-      );
+  // const onQuery = (err, data) => {
+  //   if (err) {
+  //     console.log(
+  //       'Query failed to load data. Error JSON:',
+  //       JSON.stringify(err, null, 2)
+  //     );
 
-      callback(err);
-    } else {
-      console.log('Scan succeeded.');
+  //     callback(err);
+  //   } else {
+  //     console.log('Query succeeded.');
 
-      return callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          workout: data.Items,
-        }),
-      });
-    }
-  };
+  //     if (anyinTable != null) {
+  //       console.log('before filter: ' + data.Items[0].workoutDetails);
+  //       filterResults = data.Items.filter((item) =>
+  //         item.workoutDetails
+  //           .toString()
+  //           .toLowerCase()
+  //           .includes(anyinTable.toString().toLowerCase())
+  //       );
+  //       // FilterItems(data.Items, anyinTable);
+  //       return callback(null, {
+  //         statusCode: 200,
+  //         body: JSON.stringify({
+  //           workout: filterResults,
+  //         }),
+  //       });
+  //     }
 
-  dynamoDb.scan(params, onScan);
+  //     return callback(null, {
+  //       statusCode: 200,
+  //       body: JSON.stringify({
+  //         workout: data.Items,
+  //       }),
+  //     });
+  //   }
+  // };
+
+  // dynamoDb.query(params, onQuery);
+  try {
+    const queryResult = await dynamoDb.query(params).promise();
+    console.log(queryResult.Items[0]);
+    const filterResults = FilterItems(queryResult.Items, anyinTable);
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        workout: filterResults,
+      }),
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+function FilterItems(items, queryString) {
+  let filterWorkoutDetails, filterWorkoutTime, filterWorkoutName, filterResults;
+  console.log(items[0]);
+  filterWorkoutDetails = items.filter((item) => {
+    if (item.workoutDetails != undefined) {
+      return SearchQueryString(item.workoutDetails, queryString);
+    }
+  });
+
+  filterWorkoutTime = items.filter((item) => {
+    if (item.workoutTime != undefined) {
+      return SearchQueryString(item.workoutTime, queryString);
+    }
+  });
+
+  filterWorkoutName = items.filter((item) => {
+    if (item.workoutName != undefined) {
+      return SearchQueryString(item.workoutName, queryString);
+    }
+  });
+  console.log('filterWorkoutName: ' + typeof filterWorkoutDetails);
+  console.log('filterWorkoutTime: ' + typeof filterWorkoutTime);
+  console.log('filterWorkoutDetails: ' + typeof filterWorkoutDetails);
+  filterResults = filterWorkoutDetails
+    .concat(filterWorkoutName)
+    .concat(filterWorkoutTime);
+  let set = new Set(filterResults);
+  filterResults = Array.from(set);
+  console.log('before Return' + filterResults[0]);
+  return filterResults;
+}
+
+function SearchQueryString(item, queryString) {
+  return item.toLowerCase().includes(queryString.toLowerCase());
+}
---
+++ fitness-api/model/workoutModel.js
@@ -0,0 +1,3 @@
+//workout model
+
+function getWorkoutModel() {}
